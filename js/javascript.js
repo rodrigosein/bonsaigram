e2b7c24827d957594abc4a21929952545e0c20a7
Rodrigosein <rodrigosein@gmail.com>
2024-02-19 17:55:35 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,36 @@ function redirectToProfilePage3() {
 function redirectToProfilePage4() {
     window.location.href = "./pagina-romulov/index.html";
 }
+
+
+function redirectToFotos01RodrigoS() {
+    window.location.href = "../pagina-fotos-rodrigos/caliandra.html";
+}
+
+function redirectToFotos02RodrigoS() {
+    window.location.href = "../pagina-fotos-rodrigos/buganvile.html";
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+// Avançar para a próxima imagem quando o slideshow for clicado
+document.querySelector('.slideshow-container').addEventListener('click', function() {
+    showSlides();
+});
