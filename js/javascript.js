05e68f4358ca6c45d3bf7e6562b1a8f3c4cbdefe
Rodrigosein <rodrigosein@gmail.com>
2024-02-19 14:06:34 -0300

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+function redirectToProfilePage() {
+    window.location.href = "./pagina-rodrigos/index.html";
+}
+
+function redirectToProfilePage2() {
+    window.location.href = "./pagina-rodrigov/index.html";
+}
+
+function redirectToProfilePage3() {
+    window.location.href = "./pagina-romulofv/index.html";
+}
+
+function redirectToProfilePage4() {
+    window.location.href = "./pagina-romulov/index.html";
+}
